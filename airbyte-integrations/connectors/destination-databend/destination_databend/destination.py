7fd503ccb2b255652b2c31bb12f8ac9676ca415c
hantmac <hantmac@outlook.com>
2023-02-15 12:52:08 +0800

    add https optional and upgrade dependencies

--- airbyte-integrations/connectors/destination-databend/destination_databend/destination.py
+++ airbyte-integrations/connectors/destination-databend/destination_databend/destination.py
@@ -21,7 +21,7 @@ logger = getLogger("airbyte")
 
 class DestinationDatabend(Destination):
     def write(
-        self, config: Mapping[str, Any], configured_catalog: ConfiguredAirbyteCatalog, input_messages: Iterable[AirbyteMessage]
+            self, config: Mapping[str, Any], configured_catalog: ConfiguredAirbyteCatalog, input_messages: Iterable[AirbyteMessage]
     ) -> Iterable[AirbyteMessage]:
 
         """
